a0ceea227a4ac368bcc1e719159ba5dc4fb0425d
Bryan Huang <brhuang@microsoft.com>
2017-08-19 16:19:54 -0700

    2627

---
+++ Leet/src/26_RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+        int nt = 1;
+        int i;
+        
+        if(len <= 1)
+        {
+            return len;
+        }
+
+        for(i = 1; i < len; i++)
+        {
+            if(nums[i] != nums[i-1])
+            {
+                nums[nt++] = nums[i];
+            }
+        }
+
+        return nt;
+    }
+}
---
+++ Leet/src/27_RemoveElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int pr = 0;
+        int i;
+        for(i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != val)
+           {
+               nums[pr++] = nums[i];
+           }
+        }
+
+        return pr;
+    }
+}
