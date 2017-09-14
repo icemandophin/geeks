ed73e36710fc737a9cd10e558c2b79d85258dd10
Bryan Huang <brhuang@microsoft.com>
2017-09-13 19:41:00 -0700

    91317

---
+++ Leet/src/42_TrappingRainWater.java
@@ -0,0 +1,47 @@
+/*
+DP thinking: for each vertical cell, water can hold depends on left and right high boundary (smaller one):
+fill[i] =  Min{LeftMax[i], RightMax[i]} - a[i]
+
+1. iterate L->R, calculate LeftMax[i]
+2. iterate R->L, calculate RightMax[i]
+3. iterate L->R, calculate water for each a[i]
+
+Case check:
+if a[i] > dp[i] then water[i] = 0
+for a[0] and a[N-1], left/right max is 0
+
+Further improvment: merge 2 and 3: update dp[i] and calculate final result at the same time
+*/
+
+class Solution {
+    public int trap(int[] height) {
+        int res = 0;
+        int peak = 0;
+        int N = height.length;
+        int[] dp = new int[N];
+        int i;
+
+        for (i=0; i<N; ++i)
+        {
+            dp[i] = peak;
+            peak = Math.max(peak, height[i]);
+        }
+
+        peak = 0;
+
+        for (i=N-1; i>=0; --i)
+        {
+            // find smaller one in leftMax and rightMax
+            dp[i] = Math.min(dp[i], peak);
+            // update right Max for next cell
+            peak = Math.max(peak, height[i]);
+
+            if (dp[i] > height[i])
+            {
+                res += dp[i] - height[i];
+            }
+        }
+
+        return res;
+    }
+}
---
+++ Leet/src/79_WordSearch.java
@@ -0,0 +1,61 @@
+/*
+DFS search: find 1st char, then try to match remaining word in all directions recursively
+1. {1, 0}, {-1, 0}, {0, 1}, {0, -1} to indicate 4 directions
+2. within DFS, first ensure (i, j) in boundary and value match current char (marked by index)
+3. recursively DFS 4 directions for sub-string (index+1)
+*/
+
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean [m][n];
+        int[][] move = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int i, j;
+
+        for (i=0; i<m; ++i)
+        {
+            for (j=0;j<n;++j)
+            {
+                if (dfs(board, word, i, j, visited, 0, move))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, boolean[][] visited, int index, int[][] move)
+    {
+        if (index == word.length())
+        {
+            return true;
+        }
+
+        if ((i<0) || (i>=board.length) || (j<0) || (j>=board[0].length) || (visited[i][j]) || (board[i][j]!=word.charAt(index)))
+        {
+            return false;
+        }
+
+        visited[i][j] = true;
+
+        int k, x, y;
+
+        for (k=0; k<move.length; ++k)
+        {
+            x = i + move[k][0];
+            y = j + move[k][1];
+
+            if (dfs(board, word, x, y, visited, index+1, move))
+            {
+                return true;
+            }
+        }
+
+        visited [i][j] = false;
+
+        return false;
+    }
+}
