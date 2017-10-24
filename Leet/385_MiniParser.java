c76d2f43702a99c69e52f29e18556e3501e8565f
icemandophin <iceman.dophin@gmail.com>
2017-10-24 00:35:00 -0700

    102417-1

--- Leet/385_MiniParser.java
+++ Leet/385_MiniParser.java
@@ -1,4 +1,62 @@
+/*
+iterative:
+push empty list to stack for each '['
+convert num and add to stack top/same layer
+pop stack top for each ']'
+*/
+class Solution {
+    public NestedInteger deserialize(String s) {
+        if (s == null) {
+            return null;
+        }
+        // lots of char op, covert first
+        char[] a = s.toCharArray();
+        // notice there is no space in NestedInteger
+        // if s not start with '[' => there is only one integer in a[]
+        if (a[0] != '[') {
+            return new NestedInteger(Integer.parseInt(s));
+        }
+        NestedInteger res = new NestedInteger();
+        Stack<NestedInteger> sk = new Stack<>();
+        // a[0] == '[' => push empty nested list to stack
+        sk.push(res);
+        int start = 1;
+        // traverse string array
+        for (int i = 1; i < s.length(); ++i) {
+            if (a[i] == '[') {
+                // push empty list as stack top
+                NestedInteger em = new NestedInteger();
+                sk.peek().add(em);
+                sk.push(em);
+                // move start to next index
+                start = i + 1;
+            }
+            if (a[i] == ',' || a[i] == ']') {
+                if (i - start > 0) {
+                    // convert substring to integer => to nested integer
+                    NestedInteger num = new NestedInteger(Integer.parseInt(s.substring(start, i)));
+                    // add num to current layer/stack top
+                    sk.peek().add(num);
+                }
+                // move start to next index
+                // handle next NestedInteger
+                start = i + 1;
+                //
+                if (a[i] == ']') {
+                    sk.pop();
+                }
+            }
+        }
+        return res;
+    }
+}
 
+/*
+Recursive:
+traverse string, each time took one layer of []
+and recursively resolve remaining substring
+for pure numbers, directly add to res with others in the same layer
+*/
 class Solution {
     public NestedInteger deserialize(String s) {
         if (s == null) {
@@ -18,12 +76,14 @@ class Solution {
         while (i < a.length) {
             switch (a[i]) {
                 case '[':
-                    int left = 1;
-                    while (left > 0) {
+                    // mark # of layers - for each [ +1, for each ] -1
+                    int cnt = 1;
+                    // find index of the right ']' that match current ']'
+                    while (cnt > 0) {
                         if (a[i + 1] == '[') {
-                            left++;
+                            cnt++;
                         } else if (a[i + 1] == ']') {
-                            left--;
+                            cnt--;
                         }
                         i++;
                     }
