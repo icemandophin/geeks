07d8a69554caa334460db35fff4e34079f9fabf6
icemandophin <iceman.dophin@gmail.com>
2017-11-13 18:57:00 -0800

    111317-2

---
+++ Leet/2_AddTwoNumbers.java
@@ -0,0 +1,33 @@
+/*
+two pointer approach:
+add nodes from both and create new res node with carry
+l1 and l2 can have different length - set 0 for shorter ones
+remember to include last carry = 1
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int d1 = l1 == null ? 0 : l1.val;
+            int d2 = l2 == null ? 0 : l2.val;
+            int sum = d1 + d2 + carry;
+            carry = sum >= 10 ? 1 : 0;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry == 1) cur.next = new ListNode(1);
+        return dummy.next;
+    }
+}
