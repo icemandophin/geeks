3a853d1bbdf1d4ea194355fa2fac44c18d224b77
icemandophin <iceman.dophin@gmail.com>
2017-11-22 08:28:20 -0800

    112217-1

--- Int/Bloomberg/FlatteningLinkedList.java
+++ Int/Bloomberg/FlatteningLinkedList.java
@@ -1,89 +1,117 @@
 /*
-similar to merge sort:
-recursive merge cur.left and flattened cur.right list
+two pointer approach:
+top and end always point to first/last element in cur layer
+if top node has a child, then we append the child to end
+and move end to new last node
 */
-class LinkedList
-{
-    Node head;
-
-    /* Linked list Node*/
-    class Node
-    {
-        int data;
-        Node right, down;
-        Node(int data)
-        {
-            this.data = data;
-            right = null;
-            down = null;
+class LinkedList {
+	static Node head;
+	class Node {
+		int data;
+		Node next, child;
+		Node(int d) {
+			data = d;
+			next = child = null;
+		}
+	}
+    /* The main function that flattens a multilevel linked list */
+    void flattenList(Node node) {
+        /*Base case*/
+        if (node == null) {
+            return;
+        }
+        Node tmp = null;
+        /* Find tail node of first level linked list */
+        Node tail = node;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        // One by one traverse through all nodes of first level
+        // linked list till we reach the tail node
+        Node cur = node;
+        while (cur != tail) {
+            // If current node has a child
+            if (cur.child != null) {
+                // then append the child at the end of current list
+                tail.next = cur.child;
+                // and update the tail to new last node
+                tmp = cur.child;
+                while (tmp.next != null) {
+                    tmp = tmp.next;
+                }
+                tail = tmp;
+            }
+            // Change current node
+            cur = cur.next;
         }
     }
 
-    // An utility function to merge two sorted linked lists
-    Node merge(Node a, Node b)
-    {
-        // if first linked list is empty then second
-        // is the answer
-        if (a == null)
-        {
-            return b;
-        }
+	// A utility function to create a linked list with n nodes. The data
+	// of nodes is taken from arr[]. All child pointers are set as NULL
+	Node createList(int arr[], int n) {
+		Node node = null;
+		Node p = null;
 
-        // if second linked list is empty then first
-        // is the result
-        if (b == null)
-        {
-            return a;
-        }
+		int i;
+		for (i = 0; i < n; ++i) {
+			if (node == null) {
+				node = p = new Node(arr[i]);
+			} else {
+				p.next = new Node(arr[i]);
+				p = p.next;
+			}
+			p.next = p.child = null;
+		}
+		return node;
+	}
 
-        // compare the data members of the two linked lists
-        // and put the larger one in the result
-        Node result;
+	// A utility function to print all nodes of a linked list
+	void printList(Node node) {
+		while (node != null) {
+			System.out.print(node.data + " ");
+			node = node.next;
+		}
+		System.out.println("");
+	}
 
-        if (a.data < b.data)
-        {
-            result = a;
-            result.down =  merge(a.down, b);
-        }
-        else
-        {
-            result = b;
-            result.down = merge(a, b.down);
-        }
+	Node createList() {
+		int arr1[] = new int[]{10, 5, 12, 7, 11};
+		int arr2[] = new int[]{4, 20, 13};
+		int arr3[] = new int[]{17, 6};
+		int arr4[] = new int[]{9, 8};
+		int arr5[] = new int[]{19, 15};
+		int arr6[] = new int[]{2};
+		int arr7[] = new int[]{16};
+		int arr8[] = new int[]{3};
 
-        return result;
-    }
+		/* create 8 linked lists */
+		Node head1 = createList(arr1, arr1.length);
+		Node head2 = createList(arr2, arr2.length);
+		Node head3 = createList(arr3, arr3.length);
+		Node head4 = createList(arr4, arr4.length);
+		Node head5 = createList(arr5, arr5.length);
+		Node head6 = createList(arr6, arr6.length);
+		Node head7 = createList(arr7, arr7.length);
+		Node head8 = createList(arr8, arr8.length);
 
-    Node flatten(Node root)
-    {
-        // Base Cases
-        if (root == null || root.right == null)
-            return root;
+		/* modify child pointers to create the list shown above */
+		head1.child = head2;
+		head1.next.next.next.child = head3;
+		head3.child = head4;
+		head4.child = head5;
+		head2.next.child = head6;
+		head2.next.next.child = head7;
+		head7.child = head8;
 
-        // recur for list on right
-        root.right = flatten(root.right);
+		/* Return head pointer of first linked list. Note that all nodes are
+		reachable from head1 */
+		return head1;
+	}
 
-        // now merge
-        root = merge(root, root.right);
-
-        // return the root
-        // it will be in turn merged with its left
-        return root;
-    }
-
-    /* Utility function to insert a node at begining of the linked list */
-    Node push(Node head_ref, int data)
-    {
-        /* 1 & 2: Allocate the Node & Put in the data*/
-        Node new_node = new Node(data);
-
-        /* 3. Make next of new Node as head */
-        new_node.down = head_ref;
-
-        /* 4. Move the head to point to new Node */
-        head_ref = new_node;
-
-        /*5. return to link it back */
-        return head_ref;
-    }
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		head = list.createList();
+		list.flattenList(head);
+		list.printList(head);
+	}
 }
