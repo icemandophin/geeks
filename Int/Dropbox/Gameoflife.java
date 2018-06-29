af707c8151094690deca225a3b3225acea00375f
icemandophin <iceman.dophin@gmail.com>
2018-06-29 12:56:25 -0700

    062918-2

--- Int/Dropbox/Gameoflife.java
+++ Int/Dropbox/Gameoflife.java
@@ -1,52 +1,54 @@
+/*
+in place approach:
+1. check:
+alive: 1 + 2 = 3
+reproduce: 0 + 2 = 2;
+dead: 0
+dying: 1
+2. update: b[i][j] / 2
+alive/reproduce => 1
+dead/dying => 0
+*/
 public class GameofLife {
-    public void GameOfLife(int[][] board)
-    {
-        int r = board.length;
-        int c = board[0].length;
-        for (int i = 0; i <= r; i++)
-        {
-            for (int j = 0; j <= c; j++)
-            {
-                int lives = calNumberofLives(board, i, j, r , c);
-
-                if (board[i][j]==1 && (lives < 2 || lives > 3))
-                board[i][j] = 2;
-                    else if (board[i][j]==0 && lives == 3)
-                board[i][j] = 3;
-            }
+    public void gameOfLife(int[][] b) {
+        if (b.length == 0 || b[0].length == 0) {
+            return;
         }
-        for (int i = 0; i <= r; i++)
-        {
-            for (int j = 0; j <= c; j++)
-            {
-                if (board[i][j]%2 == 1)
-                board[i][ j] = 1;
-                    else
-                {
-                    board[i][j] = 0;
+
+        int m = b.length;
+        int n = b[0].length;
+
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                // count # of neighbor lives
+                int cnt = 0;
+                for (int k = 0; k < 8; ++k) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+
+                    if (x >= 0 && x < m && y >= 0 && y < n) {
+                        cnt += b[x][y] & 1;
+                    }
+                }
+                // for cell that keep alive 1 -> 3
+                // for cell that reproduce 0 -> 2
+                // so that b[i][j] /2 = 1
+                if ((b[i][j] == 0 && cnt == 3) || (b[i][j] == 1 && (cnt == 2 || cnt == 3))) {
+                    b[i][j] += 2;
                 }
             }
         }
-
-    }
-
-    public int calNumberofLives(int[][] board, int i, int j, int r, int c)
-    {
-        int lives = 0;
-        for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1, r - 1); x++)
-        {
-            for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1, c - 1); y++)
-            {
-                if (x != i || y != j)
-                {
-                    if (board[x][y] == 1 || board[x][ y] == 2)
-                    lives++;
-                }
-
+        // for cell that dead or over-population => (1/0) / 2 = 0
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                b[i][j] >>= 1;
             }
         }
-        return lives;
     }
+
     public void GameofLife2()
     {
         int line =0;
--- Int/Dropbox/HitCounter.java
+++ Int/Dropbox/HitCounter.java
@@ -1,123 +1,77 @@
-//use queue
+/*
+Design a hit counter which counts the number of hits received in the past 5 minutes.
+Each function accepts a timestamp parameter (in seconds granularity) and you may
+assume that calls are being made to the system in chronological order
+(ie, the timestamp is monotonically increasing). You may assume that the earliest
+timestamp starts at 1.
+It is possible that several hits arrive roughly at the same time.
+*/
+
+/*
+Producer - Consumer model => message queue
+5min is condition to dequeue dated message
+hit: enqueue hit as timestamp
+count: dequeue all msg that older than 5min from head, then return queue size
+*/
 public class HitCounter {
+    private Queue<Integer> q;
 
-    const int past5Min = 300;
-        Queue<int> queue;
-        /** Initialize your data structure here. */
-        public HitCounter()
-        {
-            queue = new Queue<int>();
-        }
-
-        /** Record a hit.
-            @param timestamp - The current timestamp (in seconds granularity). */
-        public void Hit(int timestamp)
-        {
-            queue.Enqueue(timestamp);
-        }
-
-        /** Return the number of hits in the past 5 minutes.
-            @param timestamp - The current timestamp (in seconds granularity). */
-        public int GetHits(int timestamp)
-        {
-            while(queue.Count>0)
-            {
-                if(timestamp - queue.Peek()>=past5Min)
-                {
-                    queue.Dequeue();
-                }
-                else
-                    break;
-            }
-            return queue.Count;
-        }
-}
-
-//user queue and hashMap
-public class DesignHitCounter {
-
-    Deque<Integer> queue;
-    HashMap<Integer, Integer> hashMap;
-    int times = 300;
-    /** Initialize your data structure here. */
-    private static Logger LOGGER = Logger.getLogger("InfoLogging");
-    public DesignHitCounter() {
-        queue = new ArrayDeque<>();
-        hashMap = new HashMap<>();
-    }
-
-    /** Record a hit.
-     @param timestamp - The current timestamp (in seconds granularity). */
-    public void hit(int timestamp) {
-
-        if(queue.size()>0&&queue.getFirst() == timestamp)
-        {
-            hashMap.put(timestamp, hashMap.get(timestamp)+1);
-        }
-        else
-        {
-            queue.push(timestamp);
-            hashMap.put(timestamp, 1);
-        }
-    }
-
-    /** Return the number of hits in the past 5 minutes.
-     @param timestamp - The current timestamp (in seconds granularity). */
-    public int getHits(int timestamp) {
-        while (queue.size()>0)
-        {
-            LOGGER.info(queue.getLast().toString());
-            if(timestamp-queue.getLast()>=times)
-            {
-                int key = queue.pollLast();
-                hashMap.remove(key);
-            }
-            else
-                break;
-        }
-        int count = 0;
-        for (Integer key:hashMap.keySet()
-             ) {count+=hashMap.get(key);
-
-        }
-        return count;
-    }
-}
-//use two array
-public class HitCounter {
-
-    int[] hits;
-    int[] times;
-    /** Initialize your data structure here. */
     public HitCounter() {
-        hits = new int[300];
-        times = new int[300];
+        q = new LinkedList<>();
     }
 
-    /** Record a hit.
-     @param timestamp - The current timestamp (in seconds granularity). */
     public void hit(int timestamp) {
-        int value = timestamp%300;
-        if(times[value]!=timestamp)
-        {
-            times[value] = timestamp;
-            hits[value] =1;
+        q.offer(timestamp);
+    }
+    // pull "expired" hit when getting hit counts
+    public int getHits(int timestamp) {
+        while (!q.empty() && timestamp - queue.peek() >= 300) {
+            q.pull();
         }
-        else
-        {
-            hits[value]++;
+        return q.size();
+    }
+}
+
+/*
+large hit in short time optimization:
+5min = 300s => rolling array/bucket time[300] and hit[300]
+hit: check incoming time[timestamp % 300] == timestamp
+Y: multiple hits coming in together, increase hit[timestamp % 300]
+N: last hit at this spot came in 5 min ago, reset time[] and hit[]
+count: summ all hit[i] whose time[i] < current timestamp
+*/
+public class HitCounter {
+    private int[] time;
+    private int[] hit;
+
+    public HitCounter() {
+        time = new int[300];
+        hit = new int[300];
+    }
+
+    public void hit(int timestamp) {
+        int i = timestamp % 300;
+        if (time[i] != timestamp) {
+            // it has been more than 300s since last hit of this time bucket
+            // need reset outdated time/hit
+            time[i] = timestamp;
+            hit[i] = 1;
+        }
+        else {
+            // new hit arrive at the same time unit
+            // just add hit count
+            hit[i]++;
         }
     }
 
-    /** Return the number of hits in the past 5 minutes.
-     @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
-        int count = 0;
-        for(int i =0; i<300; i++)
-        {
-            if(timestamp-times[i]<300)
-                count+=hits[i];
+        int res = 0;
+        for (int i = 0; i < 300; ++i) {
+            // find all hit buckets that has time stamp within 300s
+            if (timestamp - time[i] < 300) {
+                res += hit[i];
+            }
         }
-        return count;
+        return res;
     }
 }
